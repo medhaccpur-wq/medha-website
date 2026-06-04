a8640439f892b5f4aacceb73e0ae95181da9b8fa
medhaccpur-wq <medhaccpur@gmail.com>
2026-06-04 14:08:08 +0530

    Update firebase-config.js

--- teacher/teacher/js/firebase-config.js
+++ teacher/teacher/js/firebase-config.js
@@ -1,1 +1,19 @@
-// Firebase configuration will go here later
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "medha-portal.firebaseapp.com",
+  projectId: "medha-portal",
+  storageBucket: "medha-portal.firebasestorage.app",
+  messagingSenderId: "409872731570",
+  appId: "1:409872731570:web:95780e9725a903b0a468ab",
+  measurementId: "G-SDVBPEQ1G3"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
