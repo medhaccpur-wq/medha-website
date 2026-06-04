3917bc9f1d8706c588470da9a914916d1505e5e2
medhaccpur-wq <medhaccpur@gmail.com>
2026-06-04 14:38:40 +0530

    Update auth.js

--- teacher/teacher/js/auth.js
+++ teacher/teacher/js/auth.js
@@ -1,1 +1,28 @@
-// Authentication logic will go here
+import { auth } from "./firebase-config.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", async () => {
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+
+        await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+        window.location.href = "./teacher/dashboard.html";
+
+    } catch (error) {
+
+        document.getElementById("message").innerText =
+            "Login Failed : " + error.message;
+
+    }
+
+});
